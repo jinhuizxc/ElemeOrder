e13468e4efdf62f3053d046cd4dbcf3b81c10c9a
jinhuizxc <1004260403@qq.com>
2018-09-12 15:54:31 +0800

    Initial commit

---
+++ app/src/main/java/com/jh/elemeorder/MainActivity.java
@@ -0,0 +1,17 @@
+package com.jh.elemeorder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 饿了么点餐页面
+ * https://github.com/wudifamo/Neleme
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/jh/elemeorder/bean/TypeBean.java
@@ -0,0 +1,15 @@
+package com.jh.elemeorder.bean;
+
+
+public class TypeBean {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
