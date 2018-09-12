88e9bc8d9007bc3ea51822cb9d8785c3a579f2a0
jinhuizxc <1004260403@qq.com>
2018-09-12 19:31:55 +0800

    引入依赖库

--- app/src/main/java/com/jh/elemeorder/fragment/FirstFragment.java
+++ app/src/main/java/com/jh/elemeorder/fragment/FirstFragment.java
@@ -1,9 +1,11 @@
 package com.jh.elemeorder.fragment;
 
+import com.shizhefei.fragment.LazyFragment;
+
 /**
  * Created by jinhui on 2018/9/12.
  * email: 1004260403@qq.com
  */
 
-public class FirstFragment extends Lazy{
+public class FirstFragment extends LazyFragment {
 }
